a8f2e371bfd310e963146b897271d93870148b19
nagar2817 <rohitkptn@gmail.com>
2025-02-01 14:58:41 +0530

    cors

--- server/main.py
+++ server/main.py
@@ -1,13 +1,12 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-import os
 
 app = FastAPI()
 
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[os.getenv("CORS_ORIGIN", "http://localhost:3000")],
+    allow_origins=["https://chatbox-one-alpha.vercel.app/","http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
